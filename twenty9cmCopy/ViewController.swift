6ecba0a5a5e23018c1185486f241a084c1522613
tax_k <taxfree9090@gmail.com>
2018-10-19 15:51:45 +0900

    Add ScrollView, Data, constraint

--- twenty9cmCopy/ViewController.swift
+++ twenty9cmCopy/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
+struct scrollViewDataStruct {
+    let title:String?
+    let image:UIImage?
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let scrollViewData = [scrollViewDataStruct]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    /// set horizontal gesture to control Seek
+    ///
+    /// - Parameter: (recognizer: PangestureRecognizer)
+    /// - Returns: nope
 }
 
+class CustomView: UIView {
+    let imageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.blue
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(imageView)
+        imageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("fatal Error")
+    }
+}
